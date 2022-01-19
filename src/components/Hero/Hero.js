2b05829d88f978777d795fb4bb76bb89a4e0a4cc
cecarmona <cecarmona24@gmail.com>
2022-01-18 21:58:29 -0400

    Hero Section

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,9 +5,18 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Hi I'm C2 Coder!<br />
+        JavaScript Developer
+      </SectionTitle>
+      <SectionText>
+      I am a Venezuelan software engineer with 5 years of experience in web development. I have worked for companies and projects around the world in countries such as Venezuela, Spain, Argentina, Japan, Mexico, Panama and the United States. 
+      </SectionText>
+      <Button onClick={() => window.location = 'https://google.com'}>Learn More</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
