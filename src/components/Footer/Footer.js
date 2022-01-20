90cee52461148b76c5cf2dd5ef9e1eaff8f2d713
cecarmona <cecarmona24@gmail.com>
2022-01-20 02:45:35 -0400

    Footer Animation

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,37 +4,42 @@ import { AiFillGithub, AiOutlineInstagram, AiFillLinkedin } from 'react-icons/ai
 import { SocialIcons } from '../Header/HeaderStyles';
 import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTitle, Slogan, SocialContainer, SocialIconsContainer } from './FooterStyles';
 
+// Animations
+import { Fade } from 'react-reveal';
+
 let CurrentYear = new Date().getFullYear();
 const Footer = () => {
   return (
-    <FooterWrapper>
-      <LinkList>
-        <LinkColumn>
-          <LinkTitle>Call</LinkTitle>
-          <LinkItem href="tel:+58424-180-03-04">+58 424-180-03-04</LinkItem>
-        </LinkColumn>
-        <LinkColumn>
-          <LinkTitle>Email</LinkTitle>
-          <LinkItem href="mailto:cecarmona24@gmail.com">CeCarmona24@gmail.com</LinkItem>
-        </LinkColumn>
-      </LinkList>
-      <SocialIconsContainer>
-        <CompanyContainer>
-          <Slogan>When I Code I Feel like a Rockstar © C2 Coder {CurrentYear}</Slogan>
-        </CompanyContainer>
-        <SocialContainer>
-          <SocialIcons href="https://github.com/cecarmona" target="_blank">
-            <AiFillGithub size="3rem" />
-          </SocialIcons>
-          <SocialIcons href="https://linkedin.com/in/carlosecarmonam/" target="_blank">
-            <AiFillLinkedin size="3rem" />
-          </SocialIcons>
-          <SocialIcons href="https://instagram.com/c2coder_" target="_blank">
-            <AiOutlineInstagram size="3rem" />
-          </SocialIcons>
-        </SocialContainer>
-      </SocialIconsContainer>
-    </FooterWrapper>
+    <Fade>
+      <FooterWrapper>
+        <LinkList>
+          <LinkColumn>
+            <LinkTitle>Call</LinkTitle>
+            <LinkItem href="tel:+58424-180-03-04">+58 424-180-03-04</LinkItem>
+          </LinkColumn>
+          <LinkColumn>
+            <LinkTitle>Email</LinkTitle>
+            <LinkItem href="mailto:cecarmona24@gmail.com">CeCarmona24@gmail.com</LinkItem>
+          </LinkColumn>
+        </LinkList>
+        <SocialIconsContainer>
+          <CompanyContainer>
+            <Slogan>When I Code I Feel like a Rockstar © C2 Coder {CurrentYear}</Slogan>
+          </CompanyContainer>
+          <SocialContainer>
+            <SocialIcons href="https://github.com/cecarmona" target="_blank">
+              <AiFillGithub size="3rem" />
+            </SocialIcons>
+            <SocialIcons href="https://linkedin.com/in/carlosecarmonam/" target="_blank">
+              <AiFillLinkedin size="3rem" />
+            </SocialIcons>
+            <SocialIcons href="https://instagram.com/c2coder_" target="_blank">
+              <AiOutlineInstagram size="3rem" />
+            </SocialIcons>
+          </SocialContainer>
+        </SocialIconsContainer>
+      </FooterWrapper>
+    </Fade>
   );
 };
 
