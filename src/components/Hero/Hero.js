13cb6c5b73fac958c1af462c7d6aaf92526fa1ab
cecarmona <cecarmona24@gmail.com>
2022-01-20 02:15:45 -0400

    Hero Animations

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,24 +4,33 @@ import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponent
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection, RightSection, Img } from './HeroStyles';
 
+// Animations
+import { Zoom, Fade } from 'react-reveal';
+
 const Hero = (props) => (
   <Section row nopadding>
     <LeftSection>
-      <SectionTitle main center>
-        Hi I'm C2 Coder,<br />
-        a Web Developer
-      </SectionTitle>
-      <SectionText>
-      I'm a Venezuelan software engineer with 6 years of experience in web development. I've worked for companies and projects around the world in countries such as Venezuela, Spain, Argentina, Mexico and EEUU. 
-      </SectionText>
-      <Button onClick={() => window.open('https://instagram.com/c2coder_', '_blank')} >Check My Days</Button>
+      <Zoom>
+        <SectionTitle main center>
+          Hi I'm C2 Coder,<br />
+          a Web Developer
+        </SectionTitle>
+      </Zoom>
+      <Fade bottom>
+        <SectionText>
+        I'm a Venezuelan software engineer with 6 years of experience in web development. I've worked for companies and projects around the world in countries such as Venezuela, Spain, Argentina, Mexico and EEUU. 
+        </SectionText>
+        <Button onClick={() => window.open('https://instagram.com/c2coder_', '_blank')} >Check My Days</Button>
+      </Fade>
     </LeftSection>
+    <Fade bottom>
     <RightSection>
       <SectionTitle main center style={{opacity: '0'}}>
         JavaScript Dev
       </SectionTitle>
-      <Img src="/images/cartoon-c2coder.png" />
+        <Img src="/images/cartoon-c2coder.png" />
     </RightSection>
+    </Fade>
     
   </Section>
 );
